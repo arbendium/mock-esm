a4171b4fd00f7fd6c790625fd9d4782ddef619d0
Keijo Kapp <keijo.kapp@gmail.com>
2023-10-13 14:21:37 +0300

    Updated test setup

--- test.js
+++ test.js
@@ -1,31 +1,32 @@
+import { register } from "node:module";
+register("./loader.js", import.meta.url);
+
 import assert from 'assert';
 import mock from 'mock-esm'; // has to be imported using this exact module specifier
 
-async function run() {
-	const mockValue1 = Symbol();
-	const mockValue2 = Symbol();
-	const mockValue3 = Symbol();
-	const mockValue4 = Symbol();
+const mockValue1 = Symbol();
+const mockValue2 = Symbol();
+const mockValue3 = Symbol();
+const mockValue4 = Symbol();
 
-	const { load } = mock(import.meta.url, {
-		'./test-app/mocked-module.js': {
-			default: mockValue1,
-			someExport: mockValue2
-		},
-		'./test-app/mocked-cjs-module.cjs': {
-			default: mockValue3,
-			someExport: mockValue4
-		}
-	});
+const { load } = mock(import.meta.url, {
+	'./test-app/mocked-module.js': {
+		default: mockValue1,
+		someExport: mockValue2
+	},
+	'./test-app/mocked-cjs-module.cjs': {
+		default: mockValue3,
+		someExport: mockValue4
+	}
+});
 
-	const app = await load('./test-app/index.js');
+const app = await load('./test-app/index.js');
 
-	assert.strictEqual(app.nonMockedModule.default, 'koer');
-	assert.strictEqual(app.nonMockedCjsModule.default, 'kass');
-	assert.strictEqual(app.mockedModule.default, mockValue1);
-	assert.strictEqual(app.mockedModule.someExport, mockValue2);
-	assert.strictEqual(app.mockedCjsModule.default, mockValue3);
-	assert.strictEqual(app.mockedCjsModule.someExport, mockValue4);
-}
+assert.strictEqual(app.nonMockedModule.default, 'koer');
+assert.strictEqual(app.nonMockedCjsModule.default, 'kass');
+assert.strictEqual(app.mockedModule.default, mockValue1);
+assert.strictEqual(app.mockedModule.someExport, mockValue2);
+assert.strictEqual(app.mockedCjsModule.default, mockValue3);
+assert.strictEqual(app.mockedCjsModule.someExport, mockValue4);
 
-run().catch(console.error);
+console.log('Tests have been successfully completed');
